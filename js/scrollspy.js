8ddaa32447a2fe6d1cdaa4a2e52483e249faeec9
cianjohn <83083026+cianjohn@users.noreply.github.com>
2022-10-18 11:32:01 +0100

    nav bio- hobbies/ added bio info/ setup 5 projects

--- js/scrollspy.js
+++ js/scrollspy.js
@@ -2,8 +2,9 @@
 
 const menuLinks = document.querySelectorAll("#internalNavBox .nav-link")
 const sections = document.querySelectorAll("main section")
+const Footer = document.querySelector("footer")
 // add a section margin so scroll spy moves on before section hits top of screen size is in pixels
-const sectionMargin = 200
+const sectionMargin = 100
 
 
 window.addEventListener("scroll", () => {
@@ -13,12 +14,15 @@ window.addEventListener("scroll", () => {
             }
         })
         if (window.scrollY>sections[0].offsetTop - sectionMargin && window.scrollY<=sections[1].offsetTop-sectionMargin){
-            menuLinks[0].classList.add("active")
-            console.log(window.scrollY)
-        } else if (window.scrollY>sections[1].offsetTop - sectionMargin && window.scrollY<=sections[2].offsetTop - sectionMargin){
-            menuLinks[1].classList.add("active")
+            menuLinks[0].classList.add("active");
+        } 
+        else if (window.scrollY>sections[1].offsetTop - sectionMargin && window.scrollY<=sections[2].offsetTop - sectionMargin){
+            menuLinks[1].classList.add("active");
         }
-        else if (window.scrollY>sections[2].offsetTop - sectionMargin && window.scrollY<=sections[3].offsetTop - sectionMargin){
-            menuLinks[2].classList.add("active")
+        else if (window.scrollY>sections[2].offsetTop - sectionMargin && window.scrollY<=Footer.offsetTop - sectionMargin){
+            menuLinks[2].classList.add("active");
         }
+        // else if (window.scrollY+window.innerHeight>=document.body.offsetHeight){
+        //     menuLinks[3].classList.add("active");
+        // }
 })
